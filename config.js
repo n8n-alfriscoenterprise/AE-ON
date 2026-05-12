f60f9adc3e9f6d959fbda3d58bcc09405d22927e
n8n-alfriscoenterprise <alfriscoenterprise@gmail.com>
2026-05-13 01:38:05 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// ── CONFIG ──
+const WEBHOOK    = 'https://script.google.com/macros/s/AKfycbwcRxa4BN8qvf294HWuhtCRuDaX1yH9LB_W-eCdmkmesyWcO5dMbwPQoCMdkHx83a_thA/exec';
+const API_SECRET = 'ALF-1b9KshUR1pds6qKb7jwaYEKF';
